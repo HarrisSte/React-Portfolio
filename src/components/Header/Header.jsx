a8a79f39231f3904861fd433a25a06168dda38f2
Stephanie <126029841+HarrisSte@users.noreply.github.com>
2023-11-07 22:36:58 -0500

    fix: nav links now highlight when active

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,28 +1,55 @@
 import { Container, Nav, Navbar, NavDropdown } from 'react-bootstrap';
+import { Link, useLocation } from 'react-router-dom';
 import Initials from '../../assets/ReactPortfolioINITIALS.png';
 
 import './Header.css';
 
 function Header() {
+  const location = useLocation();
+
   return (
     <Navbar expand='lg' className='nav-bar'>
       <Container>
         <div className='logo'>
-          <a href='/'>
+          <Link to='/'>
             <img src={Initials} width={120} height={120} />
-          </a>
+          </Link>
         </div>
-        <Navbar.Brand href='/'>
+        <Navbar.Brand as={Link} to='/'>
           Stephanie Harris <br></br>
           <p>Full-Stack Software Developer</p>
         </Navbar.Brand>
         <Navbar.Toggle />
         <Navbar.Collapse>
           <Nav className='ml-auto'>
-            <Nav.Link href='/'>Welcome</Nav.Link>
-            <Nav.Link href='/about'>About</Nav.Link>
-            <Nav.Link href='/portfolio'>Portfolio</Nav.Link>
-            <Nav.Link href='/contact'>Contact</Nav.Link>
+            <Nav.Link
+              as={Link}
+              to='/'
+              className={location.pathname === '/' ? 'active' : ''}
+            >
+              Welcome
+            </Nav.Link>
+            <Nav.Link
+              as={Link}
+              to='/about'
+              className={location.pathname === '/about' ? 'active' : ''}
+            >
+              About
+            </Nav.Link>
+            <Nav.Link
+              as={Link}
+              to='/portfolio'
+              className={location.pathname === '/portfolio' ? 'active' : ''}
+            >
+              Portfolio
+            </Nav.Link>
+            <Nav.Link
+              as={Link}
+              to='/contact'
+              className={location.pathname === '/contact' ? 'active' : ''}
+            >
+              Contact
+            </Nav.Link>
             <NavDropdown title='Social Media'>
               <NavDropdown.Item href='https://www.linkedin.com/in/harrisste9/'>
                 LinkedIn
