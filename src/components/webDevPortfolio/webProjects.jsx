d34059473a2a68d96e59994d3b87074e2b30860f
Stephanie <126029841+HarrisSte@users.noreply.github.com>
2024-09-10 12:23:42 -0400

    Updated Styles

--- src/components/webDevPortfolio/webProjects.jsx
+++ src/components/webDevPortfolio/webProjects.jsx
@@ -22,6 +22,13 @@ const projects = [
     githubURL: 'https://github.com/HarrisSte/Cupcakes-by-Jenn',
   },
   {
+    img: kitchWitch,
+    title: 'The Kitchen Witch',
+    description:
+      '-- UNDER DEVELOPMENT -- Future site where users can find recipes & YouTube tutorials.',
+    githubURL: 'https://github.com/HarrisSte/HexleLich-The-Kitchen-Witch',
+  },
+  {
     img: ZooImg,
     title: 'Zooquarium',
     description: 'Search for  local accredited zoos & their animals!',
