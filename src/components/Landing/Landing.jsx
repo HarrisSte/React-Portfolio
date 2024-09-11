2ae82a1b2c7ac7d783979f9e577be558f6dd1ab2
Stephanie <126029841+HarrisSte@users.noreply.github.com>
2024-09-11 12:33:56 -0400

    fix: css file name

--- src/components/Landing/Landing.jsx
+++ src/components/Landing/Landing.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
-import './Welcome.css';
+import './Landing.css';
 
 function handleDownload() {
   const link = document.createElement('a');
