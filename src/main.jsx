e3f70605b368be0cc38a589f9aea0ef6446b9c08
Stephanie <126029841+HarrisSte@users.noreply.github.com>
2024-09-11 12:26:01 -0400

    Testing again

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Welcome from './components/welcome/welcome.jsx';
 import About from './components/About/About.jsx';
 import Credentials from './components/credentials/credentials.jsx';
-import WebDev from './components/WebDevPortfolio/WebProjects.jsx';
+import WebDev from './components/webDevPortfolio/webProjects.jsx';
 import Education from './components/EduPortfolio/EduPortfolio.jsx';
 import Contact from './components/Contact/Contact.jsx';
 
