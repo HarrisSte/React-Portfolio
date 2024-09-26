1145b8a218db08f79fc0f63806117fdaa9be8b88
Stephanie <st3phanie.harris@gmail.com>
2024-09-26 14:39:51 -0400

    chore: cleaned up code structure

--- src/components/Landing/Landing.jsx
+++ src/components/Landing/Landing.jsx
@@ -2,14 +2,14 @@ import React from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
 import './Landing.css';
 
-function handleDownload() {
+const handleDownload = () => {
   const link = document.createElement('a');
   link.href = '/Resume.pdf';
   link.download = 'Resume.pdf';
   link.click();
-}
+};
 
-function Welcome() {
+const Welcome = () => {
   return (
     <Container>
       <Row>
@@ -18,16 +18,16 @@ function Welcome() {
             <span className='underline--magical'>Hey there!</span>
           </h1>
           <h2>
-            My name is Stephanie and I have over a decade in education; I have
+            My name is Stephanie, and I have over a decade in education. I have
             built a dynamic career in leadership and program management, driven
             by my passion for continuous learning. My work spans collaborating
             with diverse stakeholders, leading school-wide initiatives, and
-            organizing professional development events. <br /> <br /> I have
-            managed data-driven programs that drive improvement and track
-            progress, and I am dedicated to ongoing professional growth.
-            Adaptable and results-oriented, I excel in leading teams and
-            executing initiatives, always aiming to foster innovation, equity,
-            and excellence in education.
+            organizing professional development events.
+            <br /> <br />I have managed data-driven programs that drive
+            improvement and track progress, and I am dedicated to ongoing
+            professional growth. Adaptable and results-oriented, I excel in
+            leading teams and executing initiatives, always aiming to foster
+            innovation, equity, and excellence in education.
           </h2>
         </Col>
       </Row>
@@ -46,6 +46,6 @@ function Welcome() {
       </Row>
     </Container>
   );
-}
+};
 
 export default Welcome;
